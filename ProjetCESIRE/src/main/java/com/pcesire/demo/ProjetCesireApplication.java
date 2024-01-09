e5d7c0c7e110b1e6d5e13c9d551115bc1a6dd840
Emmanuel GARNIER BOIDUN <115170529+GARNIER-Emmanuel@users.noreply.github.com>
2024-01-09 11:03:21 +0100

    Add files via upload

---
+++ ProjetCESIRE/src/main/java/com/pcesire/demo/ProjetCesireApplication.java
@@ -0,0 +1,13 @@
+package com.pcesire.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCesireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCesireApplication.class, args);
+	}
+
+}
---
+++ ProjetCESIRE/src/test/java/com/pcesire/demo/ProjetCesireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcesire.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCesireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
